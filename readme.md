3359bc512cb924a65daf19a008aed75f5ab9d28c
Brian Seymour <imbrianseymour@gmail.com>
2015-12-15 20:37:56 -0600

    readme change, prompt box, initial custom box, updated demo

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 Mbox is a quick, simple, lightweight solution to creating Alert, Confirmation, Prompt, and Custom dialogs. Inspired in many ways by [bootbox.js for Bootstrap](https://github.com/makeusabrew/bootbox), Mbox is designed to work in the same way, but with [MaterializeCSS](https://github.com/dogfalo/materialize) as its frontend framework.
 
-Mbox is not a DOM terrorist. Whatever windows and wrappers it creates, it removes them once they are done being used. Mbox isolates itself in a self-executing function, with `jQuery` as its parameter, so even if you don't use `$`, it will still work fine.
+Mbox is not a DOM terrorist. Whatever windows and wrappers it creates, it removes them once they are done being used.
 
 #### CDN
 ```html
@@ -36,12 +36,30 @@ mbox.prompt('Did you star Mbox yet? (yes/no)', function(answer) {
     if (answer === 'yes') {
         console.log('Rock on...')
     } else {
-        console.log('Shame');
+        console.log('Shame')
     }
 })
 ```
 
 __Custom__
 ```js
-// todo
+mbox.custom({
+    message: 'What is your favorite type of pie?',
+    buttons: [
+        {
+            label: 'Pumpkin',
+            color: 'orange darken-2',
+            callback: function() {
+                mbox.alert('Pumpkin is your favorite');
+            }
+        },
+        {
+            label: 'Apple',
+            color: 'red darken-2',
+            callback: function() {
+                mbox.alert('Apple is your favorite');
+            }
+        }
+    ]
+});
 ```
--- src/mbox-0.0.1.js
+++ src/mbox-0.0.1.js
@@ -24,12 +24,23 @@ THE SOFTWARE.
 ;(function($) {
     mbox = {
 
+        template: '' +
+            '<div class="mbox-wrapper">' +
+                '<div class="mbox z-depth-1">' +
+                    '<h5>$$$_message_$$$</h5>' +
+                    '$$$_input_$$$' +
+                    '<div class="right-align">' +
+                        '$$$_buttons_$$$' +
+                    '</div>' +
+                '</div>' +
+            '</div>',
+
         alert: function(message, cb) {
             this.open('alert', message);
 
             $('.mbox-wrapper .mbox-ok-button').click(function() {
-                cb && cb();
                 mbox.close();
+                cb && cb();
             });
         },
 
@@ -37,64 +48,143 @@ THE SOFTWARE.
             this.open('confirm', message);
 
             $('.mbox-wrapper .mbox-ok-button').click(function() {
-                cb && cb(true);
                 mbox.close();
+                cb && cb(true);
             });
 
             $('.mbox-wrapper .mbox-cancel-button').click(function() {
-                cb && cb(false);
                 mbox.close();
+                cb && cb(false);
             });
         },
 
+        prompt: function(message, cb) {
+            this.open('prompt', message);
+
+            $('.mbox-wrapper .mbox-ok-button').click(function() {
+                var entered_text = $('.mbox-wrapper input').val();
+
+                mbox.close();
+                cb && cb(entered_text);
+            });
+
+            $('.mbox-wrapper .mbox-cancel-button').click(function() {
+                mbox.close();
+                cb && cb(false);
+            });
+        },
+
+        custom: function(options) {
+            if (typeof options !== 'object') {
+                throw 'Custom box requires argument 1 to be an object';
+            }
+
+            var template = this.template;
+            template = template.replace(/\$\$\$_input_\$\$\$/gi, '<hr />');
+
+            if (!options.buttons || options.buttons.length === 0) {
+                throw 'You must provide at least 1 button';
+            }
+
+            var buttons = '';
+
+            var i = 0;
+            options.buttons.forEach(function(button) {
+                var serialized_button = 'mbox-custom-button-' + i;
+
+                buttons += mbox
+                    .gen_button(
+                        button.color || 'grey lighten-4',
+                        button.label || '',
+                        serialized_button
+                    );
+
+                ++i;
+            });
+
+            template = template.replace(/\$\$\$_message_\$\$\$/gi, options.message || '');
+            template = template.replace(/\$\$\$_buttons_\$\$\$/gi, buttons);
+
+            // prevent scrolling on the body
+            $('body')
+                .append(template)
+                .addClass('mbox-open');
+
+            i = 0;
+            options.buttons.forEach(function(button) {
+                var serialized_button = 'mbox-custom-button-' + i;
+
+                $('.' + serialized_button).click(function() {
+                    if (button.callback) {
+                        button.callback();
+                    } else {
+                        mbox.close();
+                    }
+                });
+
+                ++i;
+            });
+
+            // show the box
+            $('.mbox-wrapper').show();
+        },
+
         open: function(type, message) {
+            var template = this.template;
+            var input = '<input type="text" />';
             var buttons;
 
             switch (type) {
                 case 'alert':
-                    buttons = this.gen_button('light-blue', 'Ok', 'mbox-ok-button');
+                    buttons = this.gen_button('light-blue darken-2', 'Ok', 'mbox-ok-button');
+                    template = template.replace(/\$\$\$_input_\$\$\$/gi, '<hr />');
                     break;
 
                 case 'confirm':
-                    buttons = this.gen_button('light-blue', 'Ok', 'mbox-ok-button');
-                    buttons += this.gen_button('grey', 'Cancel', 'mbox-cancel-button');
+                    buttons = this.gen_button('light-blue darken-2', 'Ok', 'mbox-ok-button');
+                    buttons += this.gen_button('grey darken-2', 'Cancel', 'mbox-cancel-button');
+                    template = template.replace(/\$\$\$_input_\$\$\$/gi, '<hr />');
+                    break;
+
+                case 'prompt':
+                    buttons = this.gen_button('light-blue darken-2', 'Ok', 'mbox-ok-button');
+                    buttons += this.gen_button('grey darken-2', 'Cancel', 'mbox-cancel-button');
+                    template = template.replace(/\$\$\$_input_\$\$\$/gi, input);
                     break;
             }
-
-            var template = '' +
-                '<div class="mbox-wrapper">' +
-                    '<div class="mbox z-depth-1">' +
-                        '<h5>$$$_message_$$$</h5>' +
-                        '<hr />' +
-                        '<div class="right-align">' +
-                            '$$$_buttons_$$$' +
-                        '</div>' +
-                    '</div>' +
-                '</div>';
 
             if (message) template = template.replace(/\$\$\$_message_\$\$\$/gi, message);
             if (buttons) template = template.replace(/\$\$\$_buttons_\$\$\$/gi, buttons);
 
+            // prevent scrolling on the body
             $('body')
                 .append(template)
                 .addClass('mbox-open');
-            $('.mbox-wrapper')
-                .show();
+
+            // show the box
+            $('.mbox-wrapper').show();
         },
 
         close: function() {
+            // hide the box
             $('.mbox')
                 .hide(0, function() {
                     $(this).closest('.mbox-wrapper').remove();
                 });
-            $('body')
-                .removeClass('mbox-open');
+
+            // allow scrolling on body again
+            $('body').removeClass('mbox-open');
+
+            // unbind all the mbox buttons
+            $('.mbox-button').unbind('click');
         },
 
         gen_button: function(color, text, type) {
             return '&nbsp;<button '+
                 'type="button" '+
-                'class="'+type+' waves-effect waves-light btn '+color+' darken-2">'+text+'</button>';
+                'class="mbox-button ' + type + ' '+
+                'waves-effect waves-light btn ' + color + '">' + text +
+                '</button>';
         }
 
     };
